64262c1d3b2beb5caa5e5378c237413ac6821177
SUMANTHTPs <sumanthtps@gmail.com>
2023-07-27 00:33:38 +0530

    updating cors origin rules

--- server.js
+++ server.js
@@ -13,22 +13,6 @@ console.log(corsOrigin);
 app.use(cors({ credentials: true, origin: corsOrigin }));
 
 
-// const allowedOrigins = ['http://localhost:3000/', 'https://localhost:3000/'];
-
-// const corsOptions = {
-//   credentials: true, // Allow cookies to be sent with the request
-//   origin: (origin, callback) => {
-//     // Check if the origin is in the allowed origins list, or if it's not provided (for server-to-server communication)
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
-
-// app.use(cors(corsOptions));
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload({
